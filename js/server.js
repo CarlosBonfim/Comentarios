e0856c7305b8bc8af45784b48f22289f5d4e09d0
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-05-12 21:14:11 -0400

    criada a rota post para inserção no banco
    tambem referenciado body-parser para lidar com as requisições do corpo

--- js/server.js
+++ js/server.js
@@ -1,17 +1,26 @@
 const {db, Postagem} = require('./connection')
 const express = require('express')
 const app = express()
-
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const port = 3000;
 
+app.use(bodyParser.json());
 app.use(cors());
 
 
 app.get('/posts', (req,res) => {
   Postagem.find()
     .then(postagens => res.send(postagens))
+    .catch(err => res.status(500).send(err))
+})
+
+app.post('/posts', (req, res) => {
+  const autor = req.body.autor;
+  const texto = req.body.texto;
+  Postagem.create({autor: autor, texto: texto})
+    .then(result =>  res.status(201).send(result))
     .catch(err => res.status(500).send(err))
 })
 
